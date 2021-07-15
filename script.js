bf2c4831ba7c64049d518a009eb2525f1dab82db
greg_edem <gregedem@outlook.com>
2021-07-15 10:07:04 +0000

    Created functions and added buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(firstVal, seconVal) {
+    return firstVal + seconVal;
+}
+
+
+function subtract(firstVal, secondVal) {
+    return firstVal - secondVal;
+}
+
+
+function multiply(firstVal, secondVal) {
+    return firstVal * secondVal;
+}
+
+
+function divide (firstVal, secondVal) {
+    return firstVal / secondVal;
+}
+
+
+function operate(firstVal, operator, secondVal) {
+    operator(firstVal, secondVal);
+}
