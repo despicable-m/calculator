2761b2f1c1e10ccd0f0d789eee4190fc2b36f1b5
greg_edem <gregedem@outlook.com>
2022-03-08 13:15:41 +0000

    Left it for too long without commiting
    I left the project for a while without commiting it now I don't really
    remember the thing I've worked on.  I'll now work on it again, but this
    time, I'll do a better job documenting.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const inputDisplay = document.querySelector('#input-display');
+const answerDisplay = document.querySelector('#answer-display');
+const nums = document.querySelectorAll('.digit');
+
+let initValue = 0;
+let result = 0;
+
 function add(firstVal, seconVal) {
     return firstVal + seconVal;
 }
@@ -18,6 +25,33 @@ function divide (firstVal, secondVal) {
 }
 
 
-function operate(firstVal, operator, secondVal) {
-    operator(firstVal, secondVal);
+function operate(e) {
+    const fn = e.srcElement.id;
+    initValue = inputDisplay.textContent;
+    answerDisplay.textContent = initValue;
+    inputDisplay.textContent = '';
+
+    // nums.forEach(num => num.addEventListener('click', (f) => {
+    //     inputDisplay.textContent += f.srcElement.textContent;
+    // }));
 }
+
+
+function toDisplay(e) {
+    let numToDisplay = document.querySelector(
+        `[data-id="${e.srcElement.textContent}"]`).textContent;
+    inputDisplay.textContent += numToDisplay;
+}
+
+
+nums.forEach(num => num.addEventListener('click', toDisplay));
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    inputDisplay.textContent = '';
+    answerDisplay.textContent = '';
+});
+
+const toOperate = document.querySelectorAll('.operator');
+toOperate.forEach(operator => 
+    operator.addEventListener('click', operate));
